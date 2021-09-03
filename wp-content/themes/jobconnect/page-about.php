583fcb0fc9d915b8aa3ee0a5dd379923a8efbcd7
spark_hema <hemalatha@sparktech.com.hk>
2021-09-03 17:13:36 +0530

    About page

--- wp-content/themes/jobconnect/page-about.php
+++ wp-content/themes/jobconnect/page-about.php
@@ -6,91 +6,95 @@ get_header();
 ?>
  <main id="primary" class="site-main">
 <body>
-
-    <section class="section section-bg" id="call-to-action" style="background-image: url(assets/images/banner-image-1-1920x500.jpg)">
+<?php
+  if( have_rows('slider') ):
+    while( have_rows('slider') ): the_row();
+      $background_image = get_sub_field('background_image');
+      $title = get_sub_field('title');
+      $sub_title = get_sub_field('sub_title');
+?>
+    <section class="section section-bg" id="call-to-action" style="background-image: url(<?php echo $background_image; ?>)">
         <div class="container">
             <div class="row">
                 <div class="col-lg-10 offset-lg-1">
                     <div class="cta-content">
                         <br>
                         <br>
-                        <h2>Learn more <em>About Us</em></h2>
-                        <p>Ut consectetur, metus sit amet aliquet placerat, enim est ultricies ligula</p>
+                        <h2><?php echo $title; ?></h2>
+                        <p><?php echo $sub_title; ?></p>
                     </div>
                 </div>
             </div>
         </div>
     </section>
-
+<?php
+  endwhile;
+  endif;
+?>
     <!-- ***** Our Classes Start ***** -->
     <section class="section" id="our-classes">
-        <div class="container">
-            <br>
-            <br>
-            <br>
-            <div class="row" id="tabs">
-              <div class="col-lg-4">
-                <ul>
-                  <li><a href='#tabs-1'><i class="fa fa-soccer-ball-o"></i> Our Goals</a></li>
-                  <li><a href='#tabs-2'><i class="fa fa-briefcase"></i> Our Work</a></a></li>
-                  <li><a href='#tabs-3'><i class="fa fa-heart"></i> Our Passion</a></a></li>
-                </ul>
+       <!-- <div  style="display:flex; padding:0px;">
+               <div class="col-lg-12" style="margin-bottom:20px;">
+                  <div class="col-lg-6">
+                      <p>Internationljobconnect was created by an young team with the global contacts and exposure. The team also has highly experienced academicians, industrialists, consultants, freelancers, philosophers and influencers. This team believes there is always a need for right talent whatever the situation is. There exist a bridge in getting the right talent with the faster connect. This team endeavor to create the right talent and also connect them with the right job.</p>
+                  </div>  
+                  <div class="col-lg-6">
+                    <p>Internationljobconnect was created by an young team with the global contacts and exposure. The team also has highly experienced academicians, industrialists, consultants, freelancers, philosophers and influencers. This team believes there is always a need for right talent whatever the situation is. There exist a bridge in getting the right talent with the faster connect. This team endeavor to create the right talent and also connect them with the right job.</p>
+                  </div>  
+                </div>
+     </div> -->
+        <div class="container" style="margin-top:80px;">
+
+                <div class="col-lg-12" style="margin-bottom:20px;">
+                  <h2 style="padding:20px 0px;">Why Internationaljobconnect (IJC)</h2>
+                  <p>IJC has the team that believe there exist a need of right talent what ever the economic situation is in the world. The team has both young and older and experienced group of people. We came across both need for job and need for right people. But there are less people in the market to do the right connect with international exposure. IJC team has global connections and networks, we feel why not to use our network to do something useful and inspirable. This is how the germination of IJC begins. We are starting this company to focus identifying right talents and connect with right job.</p>
+                </div>
+
+              <div class="col-lg-12" style="margin-bottom:20px;">
+              <h2 style="padding:20px 0px;">Where we can connect.</h2>
+              <p>IJC team has connection across the globe, right now we are gathering the right jobs from India, Zambia, Malawi, Zimbabwe, Tanzania, Malaysia, Singapore, New Zeland and Italy. We are sure our roots will search for waters from forthcoming years.</p>
               </div>
-              <div class="col-lg-8">
-                <section class='tabs-content'>
-                  <article id='tabs-1'>
-                    <img src="assets/images/about-image-1-940x460.jpg" alt="">
-                    <h4>Our Goals</h4>
 
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel aspernatur natus dignissimos eos quod, odio.</p>
-
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad cupiditate ullam exercitationem molestiae illum? Nam magni, saepe sint maiores vitae!</p>
-
-                    <p>Phasellus convallis mauris sed elementum vulputate. Donec posuere leo sed dui eleifend hendrerit. Sed suscipit suscipit erat, sed vehicula ligula. Aliquam ut sem fermentum sem tincidunt lacinia gravida aliquam nunc. Morbi quis erat imperdiet, molestie nunc ut, accumsan diam.</p>
-                   
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi suscipit commodi impedit unde accusantium nam incidunt tenetur, libero maiores enim! Nisi ex odit, totam nihil doloribus. Nemo ut, eos consequatur libero aut quas dolorum ipsa, quidem, totam dicta id possimus dolores distinctio laboriosam doloribus voluptates tenetur consectetur inventore aliquid dolorem?</p>
-                  </article>
-                  <article id='tabs-2'>
-                    <img src="assets/images/about-image-2-940x460.jpg" alt="">
-                    <h4>Our Work</h4>
-                    <p>Integer dapibus, est vel dapibus mattis, sem mauris luctus leo, ac pulvinar quam tortor a velit. Praesent ultrices erat ante, in ultricies augue ultricies faucibus. Nam tellus nibh, ullamcorper at mattis non, rhoncus sed massa. Cras quis pulvinar eros. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque ut accusantium cum! Ad quisquam, aut praesentium magni pariatur ipsa! Soluta deserunt accusantium repellendus ratione quam hic facere, cupiditate iste obcaecati a, officiis ipsum aspernatur in?</p>
-
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla a necessitatibus eos vitae quibusdam quo sunt officiis rerum voluptatibus non natus eius placeat officia vel quaerat, reprehenderit obcaecati, eaque? Repudiandae ad facere culpa accusamus aliquam ab assumenda reiciendis corrupti cum nemo, cumque molestiae corporis deserunt!</p>
-                  </article>
-                  <article id='tabs-3'>
-                    <img src="assets/images/about-image-3-940x460.jpg" alt="">
-                    <h4>Our Passion</h4>
-                    <p>Fusce laoreet malesuada rhoncus. Donec ultricies diam tortor, id auctor neque posuere sit amet. Aliquam pharetra, augue vel cursus porta, nisi tortor vulputate sapien, id scelerisque felis magna id felis. Proin neque metus, pellentesque pharetra semper vel, accumsan a neque.</p>
-
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro aut beatae commodi repudiandae distinctio, magnam blanditiis reiciendis vitae velit voluptatum natus, fugit quis eos dolores!</p>
-
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic adipisci reiciendis quaerat qui earum aut, atque esse quisquam quis exercitationem sapiente, dolorum consequatur consequuntur voluptatibus ipsam, fuga magnam beatae optio nam. Recusandae ut aliquid, eligendi.</p>
-                  </article>
-                </section>
+              <div class="col-lg-12" style="margin-bottom:20px;">
+              <h2 style="padding:20px 0px;">How we operate.</h2>
+              <p>We find the decent jobs across the globe. We will post the job in our website as and when it comes to our radar. We operate directly with the employer sometimes, such cases we take the responsibility directly. We collect resume and other details from the customers and shortlist the right candidates with carefully review by our high level HR team. We take immense care in selecting the right candidates and share the shortlisted candidates with the employer. Finally employer will decide to connect with the right candidate. We charge the candidate if he/she get the job and worked for and worked for a month time. We will get the money only after any candidate get their first month salary. We commit for the same.</p>
               </div>
-            </div>
+
         </div>
     </section>
     <!-- ***** Our Classes End ***** -->
 
-    <!-- ***** Call to Action Start ***** -->
-    <section class="section section-bg" id="call-to-action" style="background-image: url(assets/images/banner-image-1-1920x500.jpg)">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-10 offset-lg-1">
-                    <div class="cta-content">
-                        <h2>Send us a <em>message</em></h2>
-                        <p>Ut consectetur, metus sit amet aliquet placerat, enim est ultricies ligula, sit amet dapibus odio augue eget libero. Morbi tempus mauris a nisi luctus imperdiet.</p>
-                        <div class="main-button">
-                            <a href="contact.html">Contact us</a>
-                        </div>
+    <?php
+  if( have_rows('contact_message_section') ):
+    while( have_rows('contact_message_section') ): the_row();
+      $section_background = get_sub_field('section_background');
+      $title = get_sub_field('title');
+      $description = get_sub_field('description');
+      $button_label = get_sub_field('button_label');
+      $button_link = get_sub_field('button_link');
+    
+?>
+<!-- ***** Call to Action Start ***** -->
+<section class="section section-bg" id="call-to-action" style="background-image: url(<?php echo $section_background; ?>)">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-10 offset-lg-1">
+                <div class="cta-content">
+                    <h2><?php echo $title; ?></h2>
+                    <p><?php echo $description; ?></p>
+                    <div class="main-button">
+                        <a href="<?php echo $button_link; ?>"><?php echo $button_label; ?></a>
                     </div>
                 </div>
             </div>
         </div>
-    </section>
-    <!-- ***** Call to Action End ***** -->
+    </div>
+</section>
+<?php
+  endwhile;
+  endif;
+?>
+<!-- ***** Call to Action End ***** -->
     </body>
 </main><!-- #main -->
 <?php
